dbf597fbb91ead7512696ac778248bb294f938a5
ZachKopec <zachkopec@optonline.net>
2021-01-17 22:41:42 -0500

    test

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -6,7 +6,9 @@ var key = "?key=" + API_KEY
 var steamID = "&format=json&steamids=76561198036370701"
 
 // for(var i = 1; i < 11; i++)
-var fullURL = baseURL + "/" + interfaceName + "/" + methodName + "/" + methodVersion + "/" + key + "/" + steamID
+var fullURL = baseURL + "/" + interfaceName + "/" + methodName + "/" + methodVersion + "/" + key + "/" + steamID;
+
+var url = "http://api.steampowered.com/ISteamWebAPIUtil/GetSupportedAPIList/v1/?key=" + API_KEY + "&steamid=000123000456"; 
 
 d3.json(fullURL, function(response) {
 
